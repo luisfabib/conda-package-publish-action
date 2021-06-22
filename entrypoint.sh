592824231865beb216a74da572d4e00da3ac91ac
Luis Fabregas <luis.fabregas@phys.chem.ethz.ch>
2021-06-22 20:45:20 +0200

    add support for multiple platforms and architectures

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,13 +18,44 @@ check_if_meta_yaml_file_exists() {
 
 build_package(){
     conda build -c conda-forge -c bioconda --output-folder . .
+
+    if [[ $INPUT_PLATFORMS == *"osx"* ]] && [[ $INPUT_ARCH == *32* ]]; then
+    conda convert -p osx-32 linux-64/*.tar.bz2
+    fi
+    if [[ $INPUT_PLATFORMS == *"win"* ]] && [[ $INPUT_ARCH == *32* ]]; then
+    conda convert -p win-32 linux-64/*.tar.bz2
+    fi
+    if [[ $INPUT_PLATFORMS == *"osx"* ]] && [[ $INPUT_ARCH == *64* ]]; then
     conda convert -p osx-64 linux-64/*.tar.bz2
+    fi
+    if [[ $INPUT_PLATFORMS == *"win"* ]] && [[ $INPUT_ARCH == *64* ]]; then
+    conda convert -p win-64 linux-64/*.tar.bz2
+    fi
+
+
 }
 
 upload_package(){
     export ANACONDA_API_TOKEN=$INPUT_ANACONDATOKEN
-    anaconda upload --label main linux-64/*.tar.bz2
+    
+    if [[ $PLATFORMS == *"osx"* ]] && [[ $INPUT_ARCH == *32* ]]; then
+    anaconda upload --label main osx-32/*.tar.bz2
+    fi
+    if [[ $PLATFORMS == *"linux"* ]] && [[ $INPUT_ARCH == *32* ]]; then
+    anaconda upload --label main linux-32/*.tar.bz2
+    fi
+    if [[ $PLATFORMS == *"win"* ]] && [[ $INPUT_ARCH == *32* ]]; then
+    anaconda upload --label main win-32/*.tar.bz2
+    fi
+    if [[ $PLATFORMS == *"osx"* ]] && [[ $INPUT_ARCH == *64* ]]; then
     anaconda upload --label main osx-64/*.tar.bz2
+    fi
+    if [[ $PLATFORMS == *"linux"* ]] && [[ $INPUT_ARCH == *64* ]]; then
+    anaconda upload --label main linux-64/*.tar.bz2
+    fi
+    if [[ $PLATFORMS == *"win"* ]] && [[ $INPUT_ARCH == *64* ]]; then
+    anaconda upload --label main win-64/*.tar.bz2
+    fi
 }
 
 go_to_build_dir
